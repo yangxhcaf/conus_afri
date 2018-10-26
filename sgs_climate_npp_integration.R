4d471114b62fa2b5b68237e4a760fba89e89b236
Andrew Felton <felton12392@gmail.com>
2018-10-26 13:44:24 -0600

    further improvements to producing slop datasets, and combing slope map-npp datasets, sp graph code...

--- sgs_climate_npp_integration.R
+++ sgs_climate_npp_integration.R
@@ -27,8 +27,8 @@ npp_sgs_p = rasterToPoints(new_all_years_sgs); df_npp_sgs = data.frame(npp_sgs_p
 head(df_npp_sgs)
 colnames(df_npp_sgs)[3:34] <-paste(1986:2017) #rename coluns to years
 sgs_npp_melted <- melt(df_npp_sgs, 
-            id.vars = c("x", "y"),
-            variable.name = "year") #melt to long format
+                       id.vars = c("x", "y"),
+                       variable.name = "year") #melt to long format
 
 sgs_npp_melted_mean<-aggregate(value ~ x + y,mean,data=sgs_npp_melted)
 
@@ -37,8 +37,8 @@ annualprecip_sgs_p = rasterToPoints(annual_precip_allyears_sgs); df_annualprecip
 head(df_annualprecip_sgs)
 colnames(df_annualprecip_sgs)[3:32] <-paste(1986:2015) #rename columns to years
 sgs_annualprecip_melted <- melt(df_annualprecip_sgs, 
-                       id.vars = c("x", "y"),
-                       variable.name = "year") #melt to long format
+                                id.vars = c("x", "y"),
+                                variable.name = "year") #melt to long format
 
 sgs_annualprecip_melted$mm<-sgs_annualprecip_melted$value*10 #change to mm
 sgs_annualprecip_melted_mean<-aggregate(mm ~ x + y,mean,data=sgs_annualprecip_melted) #get mean values
@@ -53,19 +53,19 @@ mean_npp_precip_poly<-lm(value~mm +I(mm^2),data=merge_sgs_npp_annualprecip)
 AIC(mean_npp_precip_lm,mean_npp_precip_poly) #nonlinear relationships better
 
 #merge datasets
-merge_sgs_npp_annualprecip_allyears<-merge(sgs_annualprecip_melted,sgs_npp_melted ,by=c("x","y"))
+merge_sgs_npp_annualprecip_allyears<-merge(sgs_annualprecip_melted,sgs_npp_melted ,by=c("x","y","year"))
 head(merge_sgs_npp_annualprecip_allyears)
 
-#getting a slope value for each grid cell
-slope  <-  function(x){
-  if(all(is.na(x)))
-    # if x is all missing, then lm will throw an error that we want to avoid
-    return(NA)
-  else
-    return(coef(lm(I~x))[2])
-}
+library(dplyr)
+slope_spatial_sgs <- merge_sgs_npp_annualprecip_allyears %>% group_by(x, y) %>%
+  do(model = lm(value.y~mm, data = .)) %>%
+  mutate(coef=coef(model)[2]) xx
 
-slopetest<-aggregate(value~mm,slope,data=)
+head(slope_spatial_sgs)
+sgs_coef_only<- slope_spatial_sgs[ -c(3) ] #isolate coefficient so only slope is graphed
+head(sgs_coef_only)
+sgs_raster_coef<- rasterFromXYZ(sgs_coef_only) #convert to raster
+plot(sgs_raster_coef)
 
 #
 library(ggplot2)
@@ -96,23 +96,23 @@ ggplot(merge_sgs_npp_annualprecip,aes(mm,value,na.rm=TRUE)) +
   #geom_smooth(method="lm",se=FALSE,color="black",linetype="dashed",size=1.5) +
   #stat_smooth(method = "lm", formula = y ~ poly(x, 2), linetype="dashed",size = 1,se=FALSE,color="black") + #geom_smooth(method="lm",se=FALSE,color="black") +
   #xlab("Duration of study") +
-  #xlab("% Precipiaton deviation from control") +
-  #xlab("% Precipiaton deviation from median") +
-  #ylab("ANPP effect size") +
-  #ylab("Precipitation treatment") +
-  #geom_smooth(method="lm",se=TRUE,color="black",size=0.5) +
-  #stat_summary(fun.y = "mean",geom="point") +
-  #geom_hline(yintercept = 20,color="black",size=1) +
-  theme(
-    axis.text = element_text(color='black',size=20),
-    axis.title.x = element_text(color='black',size=22),
-    axis.title.y = element_text(color='black',size=24),
-    axis.ticks = element_line(color='black'),
-    legend.title = element_text(size=20),
-    legend.text = element_text(size=22),
-    #legend.position = c(.7,.7),
-    legend.position = c(.7,.25),
-    panel.background = element_rect(fill=NA),
-    panel.border = element_blank(), #make the borders clear in prep for just have two axes
-    axis.line.x = element_line(colour = "black"),
-    axis.line.y = element_line(colour = "black"))
+#xlab("% Precipiaton deviation from control") +
+#xlab("% Precipiaton deviation from median") +
+#ylab("ANPP effect size") +
+#ylab("Precipitation treatment") +
+#geom_smooth(method="lm",se=TRUE,color="black",size=0.5) +
+#stat_summary(fun.y = "mean",geom="point") +
+#geom_hline(yintercept = 20,color="black",size=1) +
+theme(
+  axis.text = element_text(color='black',size=20),
+  axis.title.x = element_text(color='black',size=22),
+  axis.title.y = element_text(color='black',size=24),
+  axis.ticks = element_line(color='black'),
+  legend.title = element_text(size=20),
+  legend.text = element_text(size=22),
+  #legend.position = c(.7,.7),
+  legend.position = c(.7,.25),
+  panel.background = element_rect(fill=NA),
+  panel.border = element_blank(), #make the borders clear in prep for just have two axes
+  axis.line.x = element_line(colour = "black"),
+  axis.line.y = element_line(colour = "black"))
